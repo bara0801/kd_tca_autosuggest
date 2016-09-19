9a6b882e40d360ac2fb244c144bf2efc22293dac
Kevin Ditscheid <ditscheid@engine-productions.de>
2016-09-19 09:30:34 +0200

    raise version number to 0.1.0 beta

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,38 +1,38 @@
 <?php
 
-/* 
+/*
  * This file is part of the TYPO3 CMS project.
- * 
+ *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
- * 
+ *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
- * 
+ *
  * The TYPO3 project - inspiring people to share!
  */
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Autosuggest TCA Config',
-	'description' => 'This extension provides an autosuggest TCA configuration.',
-	'category' => 'development',
-	'author' => 'Kevin Ditscheid',
-	'author_email' => 'kevinditscheid@gmail.com',
-	'author_company' => 'engine-productions GmbH',
-	'state' => 'alpha',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '0.0.1-dev',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '8.3'
+		'title' => 'Autosuggest TCA Config',
+		'description' => 'This extension provides an autosuggest TCA configuration.',
+		'category' => 'development',
+		'author' => 'Kevin Ditscheid',
+		'author_email' => 'kevinditscheid@gmail.com',
+		'author_company' => 'engine-productions GmbH',
+		'state' => 'beta',
+		'internal' => '',
+		'uploadfolder' => '0',
+		'createDirs' => '',
+		'clearCacheOnLoad' => 0,
+		'version' => '0.1.0',
+		'constraints' => array(
+				'depends' => array(
+						'typo3' => '8.3'
+				),
+				'conflicts' => array(
+				),
+				'suggests' => array(
+				),
 		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
 );
